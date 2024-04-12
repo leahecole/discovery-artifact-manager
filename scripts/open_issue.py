4bda68e33dda4bb643dba5dae7e6ed40ec9cf168
Leah Cole <coleleah@google.com>
2024-04-12 12:16:49 -0400

    change pritn to loggin

--- scripts/open_issue.py
+++ scripts/open_issue.py
@@ -158,7 +158,7 @@ def create_issue() -> str:
             time.sleep(count + 1)
         time.sleep(5)
     elif result.stderr:
-        print("resultstderr", result.stderr)
+        logging.error("resultstderr", result.stderr)
         raise subprocess.CalledProcessError(
                 returncode = result.returncode,
                 cmd = result.args,
